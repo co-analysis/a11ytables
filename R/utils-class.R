930570327625dc5f9475a3943134ecc21c177cad
Matt Dray <mwdray@gmail.com>
2022-04-19 17:15:02 +0100

    Add warnings for table count in contents, blank cells

--- R/utils-class.R
+++ R/utils-class.R
@@ -7,17 +7,17 @@
 
   names_in <- names(x)
 
-  # must be of data.frame class
+  # Must be of data.frame class
   if (!any(class(x) %in% "data.frame")) {
     stop("Input must have class data.frame.")
   }
 
-  # must have particular dimensions (must have cover, contents table, at least)
+  # Must have particular dimensions (must have cover, contents table, at least)
   if (length(names_req) != length(x) | nrow(x) < 3) {
     stop("Input must be a data.frame with 6 columns and at least 4 rows.")
   }
 
-  # column names must match expected format
+  # Column names must match expected format
   if (!all(names_req %in% names_in)) {
     stop("Input data.frame does not have the required column names.")
   }
@@ -27,12 +27,12 @@
     stop("Column 'table' must be a listcol of data.frame objects.")
   }
 
-  # class must be character for all columns except 'table'
+  # Class must be character for all columns except 'table'
   if (!all(unlist(lapply(x[-6], is.character)))) {
     stop("All columns except 'table' must be character class.")
   }
 
-  # content of listcol column must be single data.frame objects
+  # Content of listcol column must be single data.frame objects
   if (!all(unlist(lapply(x[["table"]], is.data.frame)))) {
     stop("List-column 'table' must contain data.frame objects only.")
   }
@@ -42,7 +42,18 @@
     stop("The input data.frame must have sheet_type 'cover' and 'contents'.")
   }
 
-  # there should be no empty rows for certain columns
+  # There must be only one cover, contents and (optional) notes sheet
+  if (
+    sum(x[["sheet_type"]] == "cover") > 1 |
+    sum(x[["sheet_type"]] == "contents") > 1 |
+    sum(x[["sheet_type"]] == "notes") > 1
+  ) {
+    stop(
+      "You can only have one sheet_type each of 'cover', 'contents' and 'notes'."
+    )
+  }
+
+  # There should be no empty rows for certain columns
   if (!all(unlist(lapply(x[c(1:3, 5:6)], function(x) all(!is.na(x)))))) {
     stop(
       paste(
@@ -58,15 +69,22 @@
 
   # Warn about missing sources
 
-  table_sources <- content[content$sheet_type == "tables", "source"][[1]]
+  table_sources <- content[content$sheet_type == "tables", "source"]
 
   if (any(is.na(table_sources))) {
+    warning("One of your tables is missing a source statement.", call. = FALSE)
+  }
 
+  # Warn about possibly missing rows in the contents table
+
+  contents_table <- content[content$sheet_type == "contents", "table"][[1]]
+
+  if (nrow(content) != nrow(contents_table) + 2) {
     warning(
-      "One of your tables is missing a source statement.",
+      "There are ", nrow(content) - 2, " tables but only ",
+      nrow(contents_table), " in the contents sheet.",
       call. = FALSE
     )
-
   }
 
   # Warn about notes (missing notes sheet, or notes in table)
@@ -75,8 +93,6 @@
     nrow(content[content$sheet_type == "notes", ]) > 0,
     TRUE, FALSE
   )
-
-  tables_sheets <- content[content$sheet_type == "tables", ]
 
   has_notes <-
     any(
@@ -92,12 +108,10 @@
   if (has_notes_sheet) {
 
     if (!has_notes) {
-
       warning(
         "You have a 'notes' sheet, but no notes in your tables.",
         call. = FALSE
       )
-
     }
 
   }
@@ -105,12 +119,10 @@
   if (has_notes) {
 
     if (!has_notes_sheet) {
-
       warning(
         "You have notes in your tables, but no 'notes' sheet.",
         call. = FALSE
       )
-
     }
 
   }
@@ -145,27 +157,45 @@
     not_in_notes  <- setdiff(tables_sheet_notes, notes_sheet_values)
 
     if (length(not_in_tables) > 0) {
-
       warning(
         "Some notes are in the tables (",
         paste(not_in_tables, collapse = ", "),
         ") but are missing from the notes sheet.",
         call. = FALSE
       )
-
     }
 
     if (length(not_in_notes) > 0) {
-
       warning(
         "Some notes are in the notes sheet (",
         paste(not_in_notes, collapse = ", "),
         ") but are missing from the tables.",
         call. = FALSE
       )
-
     }
 
+  }
+
+  # Warn about blank cells in tables
+
+  tables_sheets <- content[content$sheet_type == "tables", ]
+  tables_list <- setNames(tables_sheets[["table"]], tables_sheets[["tab_title"]])
+
+  tables_with_na <- unlist(
+    lapply(
+      tables_list,
+      function(x) any(!complete.cases(x))
+    )
+  )
+
+  tables_with_na_names <- names(tables_with_na[tables_with_na])
+
+  if (length(tables_with_na_names) > 0) {
+    warning(
+      "You have blank cells in these tables: ",
+      paste(tables_with_na_names, collapse = ", "), ".",
+      call. = FALSE
+    )
   }
 
 }
