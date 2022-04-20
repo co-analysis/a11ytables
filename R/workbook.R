b2e8d3ef3f34a9249b9d0a06fc56b7eb288bf544
Matt Dray <mwdray@gmail.com>
2022-04-20 11:57:17 +0100

    Use more sensible workbook file and argument names

--- R/workbook.R
+++ R/workbook.R
@@ -4,7 +4,7 @@
 #' Populate an 'openxlsx' Workbook-class object with content from an
 #' a11ytable-class object.
 #'
-#' @param content An a11ytable-class object containing the data and
+#' @param a11ytable An a11ytable-class object containing the data and
 #'     information needed to create your workbook.
 #'
 #' @details You can create an a11ytable object with
@@ -19,34 +19,34 @@
 #' }
 #'
 #' @export
-create_a11y_wb <- function(content) {
+create_a11y_wb <- function(a11ytable) {
 
-  if (!is_a11ytable(content)) {
+  if (!is_a11ytable(a11ytable)) {
     stop("The object passed to argument 'content' must have class 'a11ytable'.")
   }
 
   # Create a table_name from tab_title (unqiue, no spaces, no punctuation)
-  content[["table_name"]] <-
-    gsub(" ", "_", tolower(trimws(content[["tab_title"]])))
-  content[["table_name"]] <-
-    gsub("(?!_)[[:punct:]]", "", content[["table_name"]], perl = TRUE)
+  a11ytable[["table_name"]] <-
+    gsub(" ", "_", tolower(trimws(a11ytable[["tab_title"]])))
+  a11ytable[["table_name"]] <-
+    gsub("(?!_)[[:punct:]]", "", a11ytable[["table_name"]], perl = TRUE)
 
   # Create workbook, add tabs, cover, contents (required for all workbooks)
   wb <- openxlsx::createWorkbook()
-  wb <- .add_tabs(wb, content)
-  wb <- .add_cover(wb, content)
-  wb <- .add_contents(wb, content)
+  wb <- .add_tabs(wb, a11ytable)
+  wb <- .add_cover(wb, a11ytable)
+  wb <- .add_contents(wb, a11ytable)
 
   # There won't always be a notes tab
-  if (any(content$sheet_type %in% "notes")) {
-    wb <- .add_notes(wb, content)
+  if (any(a11ytable$sheet_type %in% "notes")) {
+    wb <- .add_notes(wb, a11ytable)
   }
 
   # Iterable titles for tabs containing tables
-  table_sheets <- content[content$sheet_type == "tables", ][["table_name"]]
+  table_sheets <- a11ytable[a11ytable$sheet_type == "tables", ][["table_name"]]
 
   for (i in table_sheets) {
-    wb <- .add_tables(wb, content, table_name = i)
+    wb <- .add_tables(wb, a11ytable, table_name = i)
   }
 
   return(wb)
