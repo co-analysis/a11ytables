8141e6edbd6ec1eedebf40d77ee1ad926706192b
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-01-17 07:53:35 +0000

    Reduce gif file size, close #121

--- data-raw/gif-vignette-workbook.R
+++ data-raw/gif-vignette-workbook.R
@@ -9,14 +9,10 @@ image_paths <- list.files(
 img_cover    <- image_read(image_paths[1])
 img_contents <- image_read(image_paths[2])
 img_notes    <- image_read(image_paths[3])
-img_table    <- image_read(image_paths[4])
+img_table_1  <- image_read(image_paths[4])
+img_table_2  <- image_read(image_paths[4])
 
-images <- c(
-  img_cover,
-  img_contents,
-  img_notes,
-  img_table
-)
+images <- c(img_cover, img_contents, img_notes, img_table_1, img_table_2)
 
 animation <- image_animate(images, fps = 1, dispose = "previous")
 
